91ca62feed8ae909abec0c0f4ba7037873b31f7c
Mehdi <mehdibigi1383@gmail.com>
2025-06-08 17:18:11 +0330

    van cards added

--- src/pages/VansPage.tsx
+++ src/pages/VansPage.tsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
 import "../../serverData";
 export default function VansPage() {
+  const [vans, SetVans] = useState([]);
+  useEffect(() => {
+    fetch("api/vans")
+      .then((res) => res.json())
+      .then((data) => SetVans(data.vans));
+  }, []);
+  const vansCard = vans.map((van) => {
+    function cardColor(car: any) {
+      if (car == "simple") return "bg-orange-400";
+      if (car == "rugged") return "bg-blue-400";
+      if (car == "luxury") return "bg-black";
+    }
+    return (
+      <div
+        key={van.id}
+        className="relative w-60 h-60 bg-orange-50 grid justify-center p-2 rounded-xl"
+      >
+        <img className="w-40" src={van.imageUrl} alt="" />
+        <div className="flex ">
+          <p className="font-bold">{van.name}</p>
+          <div className="flex flex-col ml-4">
+            <p className="font-bold">${van.price}</p>
+            <p className="font-light  -mt-1">/day</p>
+          </div>
+        </div>
+        <div
+          className={`absolute bottom-0 left-9 p-2 text-white rounded-md max-w-20 text-center ${cardColor(
+            van.type
+          )}`}
+        >
+          {van.type}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <section>
       <h2 className="text-3xl font-bold w-3/4 text-center  mx-auto mt-4">
@@ -17,6 +54,9 @@ export default function VansPage() {
         </button>
         <p className="border-b-2 cursor-pointer">clear filters</p>
       </div>
+      <main className="grid grid-cols-3 gap-5 mt-5 place-items-center">
+        {vansCard}
+      </main>
     </section>
   );
 }
