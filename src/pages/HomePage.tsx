474dc1664da399e6637ae0a0f6cd5b9b97a0ffb1
Mehdi <mehdibigi1383@gmail.com>
2025-06-08 13:02:27 +0330

    home page completed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -14,6 +14,18 @@ export default function HomePage() {
           </div>
         </nav>
       </header>
+      <main className="homepage text-white ">
+        <div className="w-full h-full z-10 relative flex flex-col gap-2 justify-center">
+          <h2 className="text-3xl font-bold w-3/4 text-left  mx-auto">
+            You got the travel plans, we got the travel vans.
+          </h2>
+          <p className=" w-3/4 text-left  mx-auto">
+            Add adventure to your life by joining the #vanlife movement. Rent
+            the perfect van to make your perfect road trip.
+          </p>
+          <button className="btn mt-5">Find your van</button>
+        </div>
+      </main>
     </section>
   );
 }
