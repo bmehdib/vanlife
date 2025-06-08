e356730d1fefc6ee7aac4b7f08746d0d83527d3f
Mehdi <mehdibigi1383@gmail.com>
2025-06-08 14:00:21 +0330

    re formatting code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import Logo from "../images/logog.png";
+import { Link } from "react-router";
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  </BrowserRouter>
+  <section className="h-svh overflow-hidden">
+    <BrowserRouter>
+      <header className="bg-orange-50 w-full h-24 p-5 grid items-center">
+        <nav className="flex justify-between">
+          <div>
+            <img className="w-40" src={Logo} alt="" />
+          </div>
+          <div className="flex gap-5 text-lg font-bold text-gray-600">
+            <Link to={"/"}>About</Link>
+            <Link to={"/"}>Vans</Link>
+          </div>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  </section>
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,16 @@
-import Logo from "../../images/logog.png";
-import { Link } from "react-router";
 export default function HomePage() {
   return (
-    <section className="w-screen max-w-[40rem]">
-      <header className="bg-white w-full h-24 p-5 grid items-center">
-        <nav className="flex justify-between">
-          <div>
-            <img className="w-40" src={Logo} alt="" />
-          </div>
-          <div className="flex gap-5 text-lg font-bold text-gray-600">
-            <Link to={"/"}>About</Link>
-            <Link to={"/"}>Vans</Link>
-          </div>
-        </nav>
-      </header>
+    <section className="w-screen h-full">
       <main className="homepage text-white ">
         <div className="w-full h-full z-10 relative flex flex-col gap-2 justify-center">
-          <h2 className="text-3xl font-bold w-3/4 text-left  mx-auto">
+          <h2 className="text-3xl font-bold w-3/4 text-center  mx-auto">
             You got the travel plans, we got the travel vans.
           </h2>
-          <p className=" w-3/4 text-left  mx-auto">
+          <p className=" w-3/4 text-center  mx-auto">
             Add adventure to your life by joining the #vanlife movement. Rent
             the perfect van to make your perfect road trip.
           </p>
-          <button className="btn mt-5">Find your van</button>
+          <button className="btn w-3/4 mt-5">Find your van</button>
         </div>
       </main>
     </section>
