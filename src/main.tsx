ef71591c94ad558258fd1e378bbb863607455c7d
Mehdi <mehdibigi1383@gmail.com>
2025-06-08 14:38:14 +0330

    about page completed

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,28 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 import Logo from "../images/logog.png";
 import { Link } from "react-router";
 createRoot(document.getElementById("root")!).render(
-  <section className="h-svh overflow-hidden">
+  <section className="h-svh">
     <BrowserRouter>
       <header className="bg-orange-50 w-full h-24 p-5 grid items-center">
         <nav className="flex justify-between">
           <div>
-            <img className="w-40" src={Logo} alt="" />
+            <Link to="/">
+              <img className="w-40" src={Logo} alt="" />
+            </Link>
           </div>
           <div className="flex gap-5 text-lg font-bold text-gray-600">
-            <Link to={"/"}>About</Link>
+            <Link to={"/about"}>About</Link>
             <Link to={"/"}>Vans</Link>
           </div>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   </section>
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,32 @@
+import bg from "../../images/bg-about.jpg";
+export default function AboutPage() {
+  return (
+    <section className="w-screen h-full">
+      <div className="w-full">
+        <img
+          className="w-full h-[25rem] object-cover object-bottom"
+          src={bg}
+          alt=""
+        />
+      </div>
+      <div className="text-white grid gap-4 mt-4 ">
+        <h2 className="text-3xl font-bold w-3/4 text-center  mx-auto">
+          Don’t squeeze in a sedan when you could relax in a van.
+        </h2>
+        <p className=" w-2/4 text-center  mx-auto mb-7">
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉) Our
+          team is full of vanlife enthusiasts who know firsthand the magic of
+          touring the world on 4 wheels.
+        </p>
+      </div>
+      <div className="text-black  w-3/4 bg-orange-50 mx-auto p-8 rounded-xl">
+        <p className="text-xl max-w-60 mb-5">
+          Your destination is waiting. Your van is ready.
+        </p>
+        <button className="bg-black text-white p-2">Explore our Vans</button>
+      </div>
+    </section>
+  );
+}
