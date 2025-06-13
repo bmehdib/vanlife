8496966c7a7474ca0429e53b966cf9eaf6edb879
Mehdi <mehdibigi1383@gmail.com>
2025-06-13 14:32:52 +0330

    fixing removed code (adding route id to vanpage)

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import VansPage from "./pages/VansPage";
 import Layout from "./pages/Layout";
 import Host from "./pages/Host";
 import Dashboard from "./components/Dashboard";
+import Van from "./components/Van";
 createRoot(document.getElementById("root")!).render(
   <section className="h-dvh overflow-x-hidden flex flex-col">
     <BrowserRouter>
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/vanspage" element={<VansPage />} />
+          <Route path="/vanspage/:id" element={<Van />} />
           <Route path="/Host" element={<Host />}>
             <Route path="/Host/Dashboard" element={<Dashboard />} />
           </Route>
