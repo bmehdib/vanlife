d838611fa9e7db1550eb1714a8cafcc4b6791c2d
Mehdi <mehdibigi1383@gmail.com>
2025-06-14 12:23:35 +0330

    header nav link now change color if is in the same route

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,18 +1,33 @@
 import Logo from "../../images/logog.png";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 export default function Header() {
   return (
     <header className="bg-orange-50 w-full p-5 grid items-center">
       <nav className="flex justify-between">
         <div>
-          <Link to="/">
+          <NavLink to="/">
             <img className="w-40" src={Logo} alt="" />
-          </Link>
+          </NavLink>
         </div>
         <div className="flex gap-5 text-lg font-bold text-gray-600">
-          <Link to={"Host"}>Host</Link>
-          <Link to={"about"}>About</Link>
-          <Link to={"vanspage"}>Vans</Link>
+          <NavLink
+            className={({ isActive }) => (isActive ? "text-orange-400" : "")}
+            to={"Host"}
+          >
+            Host
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "text-orange-400" : "")}
+            to={"about"}
+          >
+            About
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "text-orange-400" : "")}
+            to={"vanspage"}
+          >
+            Vans
+          </NavLink>
         </div>
       </nav>
     </header>
